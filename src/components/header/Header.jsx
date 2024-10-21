4f11bdfb76aec49268e94be1098f6a365f73395e
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-10-21 16:12:18 -0500

    header responsive and home main hero web

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { Box, Link, Paper, Typography, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { useLocation } from "react-router-dom";
+import DropdownMenu from "../dropdownMenu/DropdownMenu";
 
 const Header = () => {
   const theme = useTheme();
   const location = useLocation();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
     <Box
@@ -19,7 +21,8 @@ const Header = () => {
         zIndex: "999"
       }}
     >
-      <Paper
+      {!isMobile &&(
+        <Paper
         className="header"
         elevation={4}
         sx={{
@@ -164,9 +167,83 @@ const Header = () => {
             alignItems: "center"
           }}
         >
-          <Button>Contact us</Button>
+          <Link
+           className="linkButton"
+            sx={{
+              width: "10.625rem",
+              height: "3.125rem",
+              padding: 1,
+              backgroundColor: "#00A6ED",
+              color: "#FFFFFF",
+              borderRadius: "0.625rem",
+              boxShadow: "0px 0px 12.439px 0px rgba(0, 0, 0, 0.25)",
+              fontFamily: "Poppins",
+              fontSize: "1.25rem",
+              fontStyle: "normal",
+              fontWeight: "500",
+              lineHeight: "normal",
+              letterSpacing: "0.10938rem",
+              textTransform: "none",
+              textDecoration: "none",
+              textAlign: "center",
+              "&:hover": {
+                backgroundColor: "#0D1F66"
+              }
+            }}
+          >
+            Contact us
+          </Link>
         </Box>
       </Paper>
+      )}
+      {isMobile &&(
+        <Paper
+        className="header"
+        elevation={4}
+        sx={{
+          width: "98%",
+          height: "85%",
+          display: "flex",
+          justifyContent: "space-between",
+          paddingX: 5,
+          gap: 15,
+          alignItems: "center",
+          flexShrink: "0",
+          background: "rgba(153, 153, 153, 0.30)",
+          backdrodfilter: "blur(7.5px)",
+          borderRadius: "25px"
+        }}
+      >
+        <Box
+          className="logo-container"
+          sx={{
+            width: "80%",
+            height: "100%",
+            padding: 0,
+            boxSizing: "border-box"
+          }}
+        >
+          <img
+            src="/img/web-alchemy-horizontal-white-logo.png"
+            alt="web-alchemy-logo"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </Box>
+        <Box
+          className="nav-links-container"
+          sx={{
+            width: "40%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between"
+          }}
+        >
+          <DropdownMenu/>
+        </Box>
+       
+      </Paper>
+        
+      )}
     </Box>
   );
 };
